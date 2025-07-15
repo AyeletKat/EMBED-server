6e09bb4f931be99f6fc5579e728ed74402a714fb
Oriya-Sigawy <Oriyas.07@gmail.com>
2025-07-15 20:47:02 +0300

    move globals to config and put csvs in gitignore

--- App/config.py
+++ App/config.py
@@ -1,5 +1,11 @@
 import json
 
+CLINICAL_FILTERS: list[str] = ["empi_anon", "acc_anon", "tissueden", "asses", "numfind", "side", "massshape", "massmargin", "massdens", "calcfind", "calcdistri", "type", "path_severity"] #TODO: make sure that these are the names in the csv
+METADATA_FILTERS: list[str] = ["empi_anon", "acc_anon", "SeriesDescription", "FinalImageType", "ImageLateralityFinal", "ViewPosition", "spot_mag", "num_roi"]                               #TODO: make sure that these are the names in the csv
+IMAGE_PATH: list[str] = ["anon_dicom_path", "png_path", "png_filename"]
+CLINICAL_WANTED_COLUMNS: list[str] = []
+METADATA_WANTED_COLUMNS: list[str] = []
+
 def load_config():
     with open("config.json", encoding="utf-8") as f:
         return json.load(f)

6e3884aa5194969be16dc32f49e6e62ab8454aef
Oriya-Sigawy <Oriyas.07@gmail.com>
2025-07-15 20:40:47 +0300

    set the fields to filter for

---
+++ App/utils/globals.py
@@ -0,0 +1,5 @@
+CLINICAL_FILTERS: list[str] = ["empi_anon", "acc_anon", "tissueden", "asses", "numfind", "side", "massshape", "massmargin", "massdens", "calcfind", "calcdistri", "type", "path_severity"] #TODO: make sure that these are the names in the csv
+METADATA_FILTERS: list[str] = ["empi_anon", "acc_anon", "SeriesDescription", "FinalImageType", "ImageLateralityFinal", "ViewPosition", "spot_mag", "num_roi"]                               #TODO: make sure that these are the names in the csv
+IMAGE_PATH: list[str] = ["anon_dicom_path", "png_path", "png_filename"]
+CLINICAL_WANTED_COLUMNS: list[str] = []
+METADATA_WANTED_COLUMNS: list[str] = []

bed5e772838b503ba62bd2560b5667cb33c86492
Oriya-Sigawy <Oriyas.07@gmail.com>
2025-07-15 20:39:26 +0300

    csv will be in local computer

--- App/utils/data_manager.py
+++ App/utils/data_manager.py
@@ -3,20 +3,21 @@ import os
 
 # Get the path to the data folder relative to this script
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-csv_path = os.path.join(BASE_DIR, "data", "AWS_Open_Data_Clinical_Legend.csv")
+csv_path = os.path.join(BASE_DIR, "data", "AWS_Open_Data_metadata.csv")
 
 # Load the CSV
 df = pd.read_csv(csv_path)
 
-filtered_df_1 = df[df["Code"] == "1740"]
-filtered_df_2 = df[df["Header in export"] == "calcdistri"]
+filtered_df_1 = df["png_path"]
+print (filtered_df_1)
+# filtered_df_2 = df[df["Header in export"] == "calcdistri"]
 
-count_1 = len(filtered_df_1)  #suppose to be 1
-count_2 = len(filtered_df_2)  #suppose to be 6
+# count_1 = len(filtered_df_1)  #suppose to be 1
+# count_2 = len(filtered_df_2)  #suppose to be 6
 
-# Output
-print(f"Number of rows where Code == 1740: {count_1}")
-print(f"Number of rows where Header in export == calcdistri: {count_2}")
-print(filtered_df_1)
-print(filtered_df_2)
+# # Output
+# print(f"Number of rows where Code == 1740: {count_1}")
+# print(f"Number of rows where Header in export == calcdistri: {count_2}")
+# print(filtered_df_1)
+# print(filtered_df_2)
 

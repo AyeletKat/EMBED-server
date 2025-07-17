84b5d472197e1b1736f74f92bd8d5e79ab92352b
Oriya-Sigawy <Oriyas.07@gmail.com>
2025-07-17 14:44:54 +0300

    used reduced csv to see if it works (and it does)

--- App/utils/data_manager.py
+++ App/utils/data_manager.py
@@ -5,61 +5,29 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 metadata_csv_path = os.path.join(BASE_DIR, "data", "EMBED_OpenData_metadata.csv")
 clinical_csv_path = os.path.join(BASE_DIR, "data", "EMBED_OpenData_clinical.csv")
+metadata_reduced_csv_path = os.path.join(BASE_DIR, "data", "EMBED_OpenData_metadata_reduced.csv")
 
 # Load the CSV
 df_metadata = pd.read_csv(metadata_csv_path)
 df_clinical = pd.read_csv(clinical_csv_path)
+df_metadata_reduced = pd.read_csv(metadata_reduced_csv_path)
 
-# Unify types before merge
-df_clinical['acc_anon'] = df_clinical['acc_anon'].astype(str).str.strip()
-df_metadata['acc_anon'] = df_metadata['acc_anon'].astype(str).str.strip()
 
-df_clinical['empi_anon'] = df_clinical['empi_anon'].astype(str).str.strip()
-df_metadata['empi_anon'] = df_metadata['empi_anon'].astype(str).str.strip()
-
-df_clinical['side'] = df_clinical['side'].astype(str).str.upper().str.strip()
-df_metadata['ImageLateralityFinal'] = df_metadata['ImageLateralityFinal'].astype(str).str.upper().str.strip()
-
-# Step 1: Split the clinical data based on 'side' field
-df_L = df_clinical[df_clinical['side'] == 'L']
-df_R = df_clinical[df_clinical['side'] == 'R']
-df_B = df_clinical[df_clinical['side'] == 'B']
-df_NaN = df_clinical[df_clinical['side'].isna()]
-
-# Step 2: Merge L side
-merged_L = pd.merge(
-    df_L,
-    df_metadata[df_metadata['ImageLateralityFinal'] == 'L'],
+merged = pd.merge(
+    df_clinical,
+    df_metadata_reduced,
     on=['empi_anon', 'acc_anon'],
     how='inner'
 )
 
-# Step 3: Merge R side
-merged_R = pd.merge(
-    df_R,
-    df_metadata[df_metadata['ImageLateralityFinal'] == 'R'],
-    on=['empi_anon', 'acc_anon'],
-    how='inner'
+condition = (
+    merged['side'].isin(['B', 'NaN']) |
+    (merged['ImageLateralityFinal'] == merged['side'])
 )
+result = merged[condition]
+#TODO choose wanted columns
+result = result[['empi_anon', 'acc_anon', 'ImageLateralityFinal', 'side']]
+# Transpose, drop duplicate rows (i.e. duplicate columns), then transpose back
+result = result.T.drop_duplicates().T
+print(result.head(100))
 
-# Step 4: Merge B side (with all L and R images)
-merged_B = pd.merge(
-    df_B,
-    df_metadata[df_metadata['ImageLateralityFinal'].isin(['L', 'R'])],
-    on=['empi_anon', 'acc_anon'],
-    how='inner'
-)
-
-# Step 5: Merge NaN side (also with all L and R images)
-merged_NaN = pd.merge(
-    df_NaN,
-    df_metadata[df_metadata['ImageLateralityFinal'].isin(['L', 'R'])],
-    on=['empi_anon', 'acc_anon'],
-    how='inner'
-)
-
-# Step 6: Concatenate all merged DataFrames
-merged_df = pd.concat([merged_L, merged_R, merged_B, merged_NaN], ignore_index=True)
-merged_df = merged_df.head(10)
-
-print(merged_df)

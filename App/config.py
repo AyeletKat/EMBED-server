e504fd635a1939a0613d3b0b3e9c3a8392398589
Oriya-Sigawy <Oriyas.07@gmail.com>
2025-07-21 21:58:59 +0300

    decided which filters

--- App/config.py
+++ App/config.py
@@ -1,13 +1,14 @@
 import json
 
-CLINICAL_FILTERS: list[str] = ["empi_anon", "acc_anon", "tissueden", "asses", "numfind", "side", "massshape", "massmargin", "massdens", "calcfind", "calcdistri", "type", "path_severity"] #TODO: make sure that these are the names in the csv
-METADATA_FILTERS: list[str] = ["empi_anon", "acc_anon", "SeriesDescription", "FinalImageType", "ImageLateralityFinal", "ViewPosition", "spot_mag", "num_roi"]                               #TODO: make sure that these are the names in the csv
+CLINICAL_FILTERS: list[str] = ["empi_anon", "acc_anon", "tissueden", "asses", "side", "calcdistri", "path_severity"] #TODO: make sure that these are the names in the csv
+METADATA_FILTERS: list[str] = ["FinalImageType", "num_roi"]
+CLINICAL_IMAGE_DATA: list[str] = ["empi_anon", "acc_anon", "tissueden", "asses", "side", "massshape", "massmargin", "massdens", "calcdistri", "type", "path_severity"] #TODO: make sure that these are the names in the csv
+METADATA_IMAGE_DATA: list[str] = ["series_description", "num_roi", "png_filename"]   #TODO check the values each filter can get    
 IMAGE_PATH: list[str] = ["anon_dicom_path", "png_path", "png_filename"]
-CLINICAL_WANTED_COLUMNS: list[str] = []
-METADATA_WANTED_COLUMNS: list[str] = []
 
-def load_config():
-    with open("config.json", encoding="utf-8") as f:
-        return json.load(f)
+# FIXME not needed
+# def load_config():
+#     with open("config.json", encoding="utf-8") as f:
+#         return json.load(f)
 
-config = load_config()
+# config = load_config()
--- App/utils/data_manager.py
+++ App/utils/data_manager.py
@@ -5,31 +5,31 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 metadata_csv_path = os.path.join(BASE_DIR, "data", "EMBED_OpenData_metadata.csv")
 clinical_csv_path = os.path.join(BASE_DIR, "data", "EMBED_OpenData_clinical.csv")
-metadata_reduced_csv_path = os.path.join(BASE_DIR, "data", "EMBED_OpenData_metadata_reduced.csv")
 
 # Load the CSV
 df_metadata = pd.read_csv(metadata_csv_path)
 df_clinical = pd.read_csv(clinical_csv_path)
-df_metadata_reduced = pd.read_csv(metadata_reduced_csv_path)
 
+temp = df_metadata['num_roi'].drop_duplicates()
+print(temp)
 
-# merged = pd.merge(
-#     df_clinical,
-#     df_metadata,
-#     on=['empi_anon', 'acc_anon'],
-#     how='inner'
-# )
+# def merged_data():
+#     merged = pd.merge(
+#         df_clinical,
+#         df_metadata,
+#         on=['empi_anon', 'acc_anon'],
+#         how='inner'
+#     )
 
-# condition = (
-#     merged['side'].isin(['B', 'NaN']) |
-#     (merged['ImageLateralityFinal'] == merged['side'])
-# )
-# result = merged[condition]
+#     condition = (
+#         merged['side'].isin(['B', 'NaN']) |
+#         (merged['ImageLateralityFinal'] == merged['side'])
+#     )
+#     merged_df = merged[condition]
+#     return merged_df
+# def get filtered_data(filters[list[Any]]):
 # #TODO choose wanted columns
-# result = result[['empi_anon', 'acc_anon', 'ImageLateralityFinal', 'side']]
-# # Transpose, drop duplicate rows (i.e. duplicate columns), then transpose back
-# result = result.T.drop_duplicates().T
-# print(result.head(100))
-
-ex1 = df_clinical["path_severity"].drop_duplicates()
-print(ex1)
+#     merged_df = merged_df[['empi_anon', 'acc_anon', 'ImageLateralityFinal', 'side']]
+#     # Transpose, drop duplicate rows (i.e. duplicate columns), then transpose back
+#     merged_df = merged_df.T.drop_duplicates().T
+# print(merged_df.head(10))

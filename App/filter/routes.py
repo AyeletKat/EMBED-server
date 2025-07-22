f3e53d8783c2944743ef3b1bcd7f491be08abfe2
Oriya-Sigawy <Oriyas.07@gmail.com>
2025-07-22 18:30:38 +0300

    Test change

--- App/filter/routes.py
+++ App/filter/routes.py
@@ -1,13 +1,17 @@
+# This file should be deleted.
 import json
 from flask import Blueprint, jsonify
 from App import data_mng
 
+
 filter_bp = Blueprint('filter', __name__)
+#gets the options for filters
 
 @filter_bp.route('/options', methods=['GET'])
 def filter_options():
     response = data_mng.get_unique_values("common")  
     res_str = json.dumps(response, default=str)
+    print("filter options:", res_str)
     return jsonify(res_str), 200
 
 @filter_bp.route('/abnormality-options', methods=['GET'])
